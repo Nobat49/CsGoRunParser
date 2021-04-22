4ce4b32bccdb6d23112fe13316b469366a084d4f
Nobat49 <47610257+Nobat49@users.noreply.github.com>
2021-04-22 18:10:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+from time import sleep
+from ftplib import FTP
+import requests
+
+f = 1
+crashes = []
+
+for i in range(1005840,1750000):
+	if f == 100:
+		with open('crashes.txt', 'a+') as file:
+			for txt in crashes:
+				file.write(str(txt) + '\n')
+			file.close()
+		crashes = []
+		f = 1
+	try:
+		json = requests.get(f'https://api.csgorun.pro/games/{i}').json()
+		g_id = json['data']['id']
+		crash = json['data']['crash']
+		print(f'''
+			Id = {g_id}
+			Crash = {crash}
+			---------------------------
+		''')
+		crashes.append([g_id,crash])
+		f = f + 1
+	except:
+		print(f'''
+			Id = {i}
+			Crash = Error
+			''')
+		continue
+
+with open('crashes1.txt', 'a+') as file:
+	for txt in crashes:
+		file.write(str(txt) + '\n')
+	file.close()
+
+sleep(30)
+
+ftp = FTP('files.000webhost.com', 'herfiles', '2283320704')
+with open('crashes.txt', 'rb') as f:
+	ftp.storlines('STOR crashes.txt', f)
+	f.close()
+print('Upload succes')
