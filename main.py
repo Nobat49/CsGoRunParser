cf8044f5191746f95aa78a125eaaa9f696c57b22
Nobat49 <47610257+Nobat49@users.noreply.github.com>
2021-10-08 20:28:33 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 f = 1
 crashes = []
 
-for i in range(1005840,1750000):
+for i in range(300000,2212514):
 	if f == 100:
 		with open('crashes.txt', 'a+') as file:
 			for txt in crashes:
@@ -14,14 +14,25 @@ for i in range(1005840,1750000):
 		f = 1
 	try:
 		json = requests.get(f'https://api.csgorun.pro/games/{i}').json()
+
 		g_id = json['data']['id']
 		crash = json['data']['crash']
+		bets = json['data']['bets']
+		total_bet = 0.0
+
+		for i in bets:
+			bet = i['deposit']['amount']
+			total_bet = total_bet + bet
+
 		print(f'''
 			Id = {g_id}
 			Crash = {crash}
+			Total bet = {total_bet}
 			---------------------------
 		''')
-		crashes.append([g_id,crash])
+
+		crashes.append([g_id,crash,total_bet])
+
 		f = f + 1
 		sleep(0.3)
 	except:
